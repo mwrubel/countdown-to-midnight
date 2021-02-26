1d76b2e4ecee94e265ae964397fdf3f9d3d30109
Mike <mikewrubel51@gmail.com>
2021-02-26 12:37:30 -0500

    commit tree 4b3dd39bb19d4fbe1b37982da2e0fc652cba5f30

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+    seconds = 10
+    while seconds > 1
+        sleep(1)
+        seconds -= 1
+    end
+    puts "HAPPY NEW YEAR!"
 end
